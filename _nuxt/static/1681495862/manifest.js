0baad73f17bf1a1766f11395645e61e47bf55237
wesle.gcarvalho <wesley.gcarvalho@goias.gov.br>
2023-04-14 15:18:21 -0300

    4fa8df4

---
+++ _nuxt/static/1681495862/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fmembers","\u002F","\u002Fprojects"]})
---
+++ _nuxt/static/1681495862/projects/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/project-manager/projects", {data:[{}],fetch:{},mutations:void 0});
